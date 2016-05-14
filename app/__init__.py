edcdabef40845aaf188e220708092c314e68ec95
DullSmile <973915422@qq.com>
2016-05-14 08:54:10 -0700

    蓝本在工厂函数create_app()中注册到程序

--- app/__init__.py
+++ app/__init__.py
@@ -27,5 +27,7 @@ def create_app(config_name):
     db.init_app(app)
 
     # 附加路由和自定义的错误页面
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
 
     return app
