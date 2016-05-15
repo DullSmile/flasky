e65fbf110ea515f1d031cd0741092f2ec60ecf6d
DullSmile <973915422@qq.com>
2016-05-14 19:59:03 -0700

    更新数据库模型

--- app/models.py
+++ app/models.py
@@ -4,8 +4,18 @@ from app import db
 
 
 class Role(db.Model):
-    pass
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(64), unique = True)
+
+    def __repr__(self):
+        return '<Role %r>' %self.name
 
 
 class User(db.Model):
-    pass
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(64), unique = True)
+
+    def __repr__(self):
+        return '<User %r>' %self.username
