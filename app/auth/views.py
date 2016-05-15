1b5c318add08236928b0728b87fb2c4660605044
DullSmile <973915422@qq.com>
2016-05-15 00:22:31 -0700

    更新登陆路由

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,12 +1,22 @@
 #!/usr/bin/env python
 # -*-coding:utf-8-*-
-from flask import render_template
-from . import auth
+from flask import render_template, redirect, request, flash, url_for
+from flask_login import login_user
 from flask_login import login_required #8.2保护路由
+from . import auth
+from ..models import User
+from .forms import Login_Form
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('auth/login.html')
+    form = Login_Form()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user is not None and user.verify_password(form.password.data):
+            login_user(user, form.remember_me.data)
+            return redirect(request.args.get('next') or url_for('main.index'))
+        flash('Invalidd username or password.')
+    return render_template('auth/login.html', form=form)
 
 #保护路由,只允许认证用户访问
 @app.route('/secret')
