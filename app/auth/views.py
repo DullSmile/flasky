0445212478a510a626c137a03a98a5baf9cc96b6
DullSmile <973915422@qq.com>
2016-05-14 21:31:27 -0700

    保护路由,同坐Flask-Login扩展的login_requi修饰器实现

--- app/auth/views.py
+++ app/auth/views.py
@@ -2,7 +2,14 @@
 # -*-coding:utf-8-*-
 from flask import render_template
 from . import auth
+from flask_login import login_required #8.2保护路由
 
 @auth.route('/login')
 def login():
     return render_template('auth/login.html')
+
+#保护路由,只允许认证用户访问
+@app.route('/secret')
+@login_required
+def secret():
+    return 'Only authenticated users are allowed!'
