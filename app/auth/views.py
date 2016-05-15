44d169411461b7404e6b9873267d9e40fdd8e44d
DullSmile <973915422@qq.com>
2016-05-15 00:30:59 -0700

    增加登出路由

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*-coding:utf-8-*-
 from flask import render_template, redirect, request, flash, url_for
-from flask_login import login_user
+from flask_login import login_user,login_required
 from flask_login import login_required #8.2保护路由
 from . import auth
 from ..models import User
@@ -17,6 +17,13 @@ def login():
             return redirect(request.args.get('next') or url_for('main.index'))
         flash('Invalidd username or password.')
     return render_template('auth/login.html', form=form)
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('You have been logged out.')
+    return redirect(url_for('main.index'))
 
 #保护路由,只允许认证用户访问
 @app.route('/secret')
