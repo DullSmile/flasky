156715db2d715bc47fbbb532ea460a5b28ab377a
DullSmile <973915422@qq.com>
2016-05-14 19:39:35 -0700

    更行flask-moment扩展

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from flask_mail import Mail
 from flask_sqlalchemy import SQLAlchemy
-from flask.ext.moment import Moment
+from flask_moment import Moment
 from config import config
 
 
