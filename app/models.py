43a4941eac1e61a04ada487c65af3234309c1dd8
DullSmile <973915422@qq.com>
2016-05-15 07:06:15 -0700

    使用itsdangerous生成确认令牌

--- app/models.py
+++ app/models.py
@@ -4,6 +4,7 @@ from app import db
 from werkzeug.security import generate_password_hash, check_password_hash #密码散列
 from flask_login import UserMixin #认证用户
 from . import login_manager #加载用户的回调函数
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer #确认用户账户
 
 class Role(db.Model):
     __tablename__ = 'roles'
@@ -21,6 +22,7 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(64), unique = True, index = True)
     password_hash = db.Column(db.String(128))
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+    confirmed = db.Column(db.Boolean, default = False)
 
     def __repr__(self):
         return '<User %r>' %self.username
@@ -40,3 +42,21 @@ class User(UserMixin, db.Model):
     @login_manager.user_loader
     def load_user(user_id):
         return User.query.get(int(user_id))
+
+    #生成令牌,有效期为1小时
+    def generate_confirmation_token(self, expiration=3600):
+        s = Serializer(current_app.config['SECRET_KEY'], expiration)
+        return s.dumps({'confirm': self.id})
+
+    #检验令牌
+    def confirm(self, token):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data = s.load(token)
+        except:
+            return False
+        if data.get('confirm') != self.id:
+            return False
+        self.confirmed = True
+        db.session.add(self)
+        return True
