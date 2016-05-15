69ebde74abfb53c2f516a0ec9bedd794afd3064d
DullSmile <973915422@qq.com>
2016-05-14 21:24:02 -0700

    在工厂函数中初始化User模型的Login

--- app/__init__.py
+++ app/__init__.py
@@ -7,13 +7,17 @@ from flask_mail import Mail
 from flask_sqlalchemy import SQLAlchemy
 from flask_moment import Moment
 from config import config
-
+from flask_login import LoginManager
 
 bootstrap = Bootstrap()
 mail = Mail()
 moment = Moment()
 db = SQLAlchemy()
 
+#实例化login_manager
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
 
 def create_app(config_name):
     app = Flask(__name__)
@@ -33,5 +37,8 @@ def create_app(config_name):
     #将auth认证蓝本注册到create_app函数中
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint, url_prefix = '/auth')
+
+    #在工厂函数中初始化
+    login_manager.init_app(app)
 
     return app
